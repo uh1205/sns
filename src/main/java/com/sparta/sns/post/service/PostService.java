49ebb75cc8484c4a6fc75490c81354ac1f878ca2
Unho Yeo <122009132+uh1205@users.noreply.github.com>
2024-07-02 17:16:28 +0900

    Feat: Add PostService

---
+++ src/main/java/com/sparta/sns/post/service/PostService.java
@@ -0,0 +1,96 @@
+package com.sparta.sns.post.service;
+
+import com.sparta.sns.exception.PostNotFoundException;
+import com.sparta.sns.exception.UserNotFoundException;
+import com.sparta.sns.post.dto.PostRequest;
+import com.sparta.sns.post.entity.Post;
+import com.sparta.sns.post.repository.PostRepository;
+import com.sparta.sns.tag.Tag;
+import com.sparta.sns.tag.TagRepository;
+import com.sparta.sns.user.entity.User;
+import com.sparta.sns.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class PostService {
+
+    private final PostRepository postRepository;
+    private final TagRepository tagRepository;
+    private final UserRepository userRepository;
+
+    /**
+     * 게시물 작성
+     */
+    @Transactional
+    public Post createPost(PostRequest request, List<MultipartFile> files, User user) {
+        Set<Tag> tags = getTagsFromRequest(request.getTagNames());
+        Post post = Post.of(request, files, tags, user);
+        return postRepository.save(post);
+    }
+
+    /**
+     * 전체 게시물 조회
+     */
+    public Page<Post> getAllPosts(Long userId, Pageable pageable) {
+        if (userId == null) {
+            return postRepository.findAll(pageable);
+        }
+        User user = userRepository.findById(userId).orElseThrow(UserNotFoundException::new);
+        return postRepository.findAllByUser(user, pageable);
+    }
+
+    /**
+     * 특정 게시물 조회
+     */
+    public Post getPost(Long postId) {
+        return postRepository.findById(postId).orElseThrow(PostNotFoundException::new);
+    }
+
+    /**
+     * 게시물 수정
+     */
+    @Transactional
+    public Post updatePost(Long postId, PostRequest request, User user) {
+        Post post = getPost(postId);
+        post.verifyUser(user);
+        Set<Tag> tags = getTagsFromRequest(request.getTagNames());
+        post.update(request, tags);
+        return post;
+    }
+
+    /**
+     * 게시물 삭제
+     */
+    @Transactional
+    public Long deletePost(Long postId, User user) {
+        Post post = getPost(postId);
+        post.verifyUser(user);
+        postRepository.delete(post);
+        return postId;
+    }
+
+    /**
+     * PostRequest의 tagNames를 기반으로 Tag 엔티티를 조회하거나 생성하여 Set<Tag>으로 반환
+     */
+    private Set<Tag> getTagsFromRequest(List<String> tagNames) {
+        Set<Tag> tags = new HashSet<>();
+        for (String tagName : tagNames) {
+            Tag tag = tagRepository.findByName(tagName)
+                    .orElseGet(() -> tagRepository.save(new Tag(tagName)));
+            tags.add(tag);
+        }
+        return tags;
+    }
+
+}
