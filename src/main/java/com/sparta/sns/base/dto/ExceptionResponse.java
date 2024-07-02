ebf627a8164313f9a82f8d0335c43f1a32c0455d
Unho Yeo <122009132+uh1205@users.noreply.github.com>
2024-07-02 17:22:12 +0900

    Feat: Add Base DTOs

--- src/main/java/com/sparta/sns/base/dto/ExceptionResponse.java
+++ src/main/java/com/sparta/sns/base/dto/ExceptionResponse.java
@@ -3,10 +3,9 @@ package com.sparta.sns.base.dto;
 import lombok.Builder;
 
 @Builder
-public class CommonResponse<T> {
+public class ExceptionResponse {
 
     private int statusCode;
     private String msg;
-    private T data;
 
 }
