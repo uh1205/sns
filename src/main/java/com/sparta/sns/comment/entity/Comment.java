1dc6585d6bb80d87b47387a2e4aafb09309375ab
Unho Yeo <122009132+uh1205@users.noreply.github.com>
2024-07-02 19:07:44 +0900

    Feat: Add Comment Entity

---
+++ src/main/java/com/sparta/sns/comment/entity/Comment.java
@@ -0,0 +1,80 @@
+package com.sparta.sns.comment.entity;
+
+import com.sparta.sns.comment.dto.CommentRequest;
+import com.sparta.sns.exception.DifferentPostException;
+import com.sparta.sns.exception.DifferentUserException;
+import com.sparta.sns.post.entity.Post;
+import com.sparta.sns.user.entity.User;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "comment_id")
+    private Long id;
+
+    private String content;
+    private int likeCount;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    /**
+     * 생성자
+     */
+    private Comment(CommentRequest request, Post post, User user) {
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+        this.content = request.getContent();
+        this.likeCount = 0;
+        this.user = user;
+        this.post = post;
+        post.getComments().add(this);
+    }
+
+    public static Comment of(CommentRequest request, Post post, User user) {
+        return new Comment(request, post, user);
+    }
+
+    public void verifyPostId(Long postId) {
+        if (!postId.equals(this.post.getId())) {
+            throw new DifferentPostException();
+        }
+    }
+
+    public void verifyUser(User user) {
+        if (!user.getId().equals(this.user.getId())) {
+            throw new DifferentUserException();
+        }
+    }
+
+    public void update(CommentRequest request) {
+        this.content = request.getContent();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    public void increaseLikeCount() {
+        this.likeCount++;
+    }
+
+    public void decreaseLikeCount() {
+        this.likeCount--;
+    }
+
+}
