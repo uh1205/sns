2af417dab0e4e32cf36484d7ca81f161e0a911b0
Unho Yeo <122009132+uh1205@users.noreply.github.com>
2024-07-03 10:55:02 +0900

    Feat: Add TagRepository

---
+++ src/main/java/com/sparta/sns/tag/TagRepository.java
@@ -0,0 +1,11 @@
+package com.sparta.sns.tag;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface TagRepository extends JpaRepository<Tag, Long> {
+
+    Optional<Tag> findByName(String tagName);
+
+}
