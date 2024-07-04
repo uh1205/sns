652f4ba2559198da7db4c8a7d0470992c5c4720c
Unho Yeo <122009132+uh1205@users.noreply.github.com>
2024-07-04 17:04:27 +0900

    Feat: Add findTop10ByOrderByFollowersCountDesc()

--- src/main/java/com/sparta/sns/user/repository/UserRepository.java
+++ src/main/java/com/sparta/sns/user/repository/UserRepository.java
@@ -3,6 +3,7 @@ package com.sparta.sns.user.repository;
 import com.sparta.sns.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
@@ -10,5 +11,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByUsername(String username);
 
     Optional<User> findByUsername(String username);
+
+    List<User> findTop10ByOrderByFollowersCountDesc();
 
 }
