5ae57064d0383bd5456c7377b4e175733859373d
Unho Yeo <122009132+uh1205@users.noreply.github.com>
2024-07-05 15:45:49 +0900

    Refactor: Update CommentRepository

--- src/main/java/com/sparta/sns/primary/comment/Repository/CommentRepository.java
+++ src/main/java/com/sparta/sns/primary/comment/Repository/CommentRepository.java
@@ -1,13 +1,12 @@
 package com.sparta.sns.primary.comment.Repository;
 
 import com.sparta.sns.primary.comment.entity.Comment;
-import com.sparta.sns.primary.post.entity.Post;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CommentRepository extends JpaRepository<Comment, Long>, CommentRepositoryCustom {
 
-    Page<Comment> findAllByPost(Post post, Pageable pageable);
+    Page<Comment> findAllByPostId(Long postId, Pageable pageable);
 
 }
