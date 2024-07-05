b799518ecef7084f9a9e9530b6b04b2a0cdd0c3f
Unho Yeo <122009132+uh1205@users.noreply.github.com>
2024-07-05 15:46:09 +0900

    Refactor: Update FollowRepository

--- src/main/java/com/sparta/sns/primary/follow/repository/FollowRepository.java
+++ src/main/java/com/sparta/sns/primary/follow/repository/FollowRepository.java
@@ -1,14 +1,15 @@
 package com.sparta.sns.primary.follow.repository;
 
 import com.sparta.sns.primary.follow.entity.Follow;
+import com.sparta.sns.primary.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface FollowRepository extends JpaRepository<Follow, Long> {
 
-    boolean existsByFollowerIdAndFollowingId(Long followerId, Long followingId);
+    boolean existsByFollowerAndFollowing(User follower, User following);
 
-    Optional<Follow> findByFollowerIdAndFollowingId(Long followerId, Long followingId);
+    Optional<Follow> findByFollowerAndFollowing(User follower, User following);
 
 }
