a69fc75ecc5ab8eb9ed82631f9278ca9e8a6a26b
Unho Yeo <122009132+uh1205@users.noreply.github.com>
2024-07-05 15:44:47 +0900

    Refactor: Update exceptions

--- src/main/java/com/sparta/sns/secondary/exception/DifferentPostException.java
+++ src/main/java/com/sparta/sns/secondary/exception/DifferentPostException.java
@@ -3,7 +3,6 @@ package com.sparta.sns.secondary.exception;
 public class DifferentPostException extends RuntimeException {
 
     public DifferentPostException() {
-        super("게시물이 일치하지 않습니다.");
     }
 
     public DifferentPostException(String message) {
--- src/main/java/com/sparta/sns/secondary/exception/SelfUnfollowException.java
+++
@@ -1,9 +0,0 @@
-package com.sparta.sns.secondary.exception;
-
-public class SelfUnfollowException extends RuntimeException {
-
-    public SelfUnfollowException() {
-        super("본인을 언팔로우 할 수 없습니다.");
-    }
-
-}
